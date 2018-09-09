18edf399724e968c3eb45fa2883965c0cd734fc5
Hafiz Adewuyi <hoadewuyi@gmail.com>
2018-09-09 20:11:35 +0100

    Fix bug with adding and deleting word feedbacks

--- app/scripts/app/names/NamesDirective.js
+++ app/scripts/app/names/NamesDirective.js
@@ -1,167 +1,183 @@
-'use strict';
+"use strict";
 
 /* Directives */
-angular.module('NamesModule')  // Directive adds the geolocation autocompletes on the tagsInput field of Name Form
-  .directive('nameForm', [
-    'geolocationService',
-    function (geo) {
+angular
+  .module("NamesModule") // Directive adds the geolocation autocompletes on the tagsInput field of Name Form
+  .directive("nameForm", [
+    "geolocationService",
+    function(geo) {
       return {
-        link: function (scope) {
-          scope.query = function () {
+        link: function(scope) {
+          scope.query = function() {
             return geo.load();
           };
         }
       };
     }
-  ])  // Directive adds File Uploader widget on the New Name Form for uploading names in bulk
-  .directive('namesUpload', [
-    'uploadService',
-    function (Uploader) {
+  ]) // Directive adds File Uploader widget on the New Name Form for uploading names in bulk
+  .directive("namesUpload", [
+    "uploadService",
+    function(Uploader) {
       return {
-        controller: function ($scope) {
+        controller: function($scope) {
           $scope.uploader = Uploader({
-            url: '/v1/names/upload',
-            alias: 'nameFiles',
+            url: "/v1/names/upload",
+            alias: "nameFiles",
             fileType: [
-              'text/csv',
-              'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+              "text/csv",
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             ]
           });
         }
       };
     }
-  ])  // Directive adds array of Etymology fields to the Name Form
-  .directive('etymology', [
-    '$stateParams',
-    function ($stateParams) {
+  ]) // Directive adds array of Etymology fields to the Name Form
+  .directive("etymology", [
+    "$stateParams",
+    function($stateParams) {
       return {
         replace: true,
-        restrict: 'E',
-        templateUrl: 'tmpls/names/directives/etymology.html',
-        link: function (scope) {
+        restrict: "E",
+        templateUrl: "tmpls/names/directives/etymology.html",
+        link: function(scope) {
           if (!$stateParams.entry) {
             scope.word.etymology = [];
             scope.word.etymology.push({
-              part: '',
-              meaning: ''
+              part: "",
+              meaning: ""
             });
           }
 
-          scope.add_etymology = function () {
+          scope.add_etymology = function() {
             return scope.word.etymology.push({
-              part: '',
-              meaning: ''
+              part: "",
+              meaning: ""
             });
           };
 
-          scope.remove_etymology = function (index) {
+          scope.remove_etymology = function(index) {
             scope.word.etymology.splice(index, 1);
             if (scope.word.etymology.length < 1)
               return scope.word.etymology.push({
-                part: '',
-                meaning: ''
+                part: "",
+                meaning: ""
               });
           };
-          scope.$watch('name.etymology', function () {
-            scope.form.$dirty = true;
-          }, true);
+          scope.$watch(
+            "name.etymology",
+            function() {
+              scope.form.$dirty = true;
+            },
+            true
+          );
         }
       };
     }
   ]) // Directive adds array of definition fields to the word Form
-  .directive('definition', [
-    '$stateParams',
-    function ($stateParams) {
+  .directive("definition", [
+    "$stateParams",
+    function($stateParams) {
       return {
         replace: true,
-        restrict: 'E',
-        templateUrl: 'tmpls/names/directives/definition.html',
-        link: function (scope) {
+        restrict: "E",
+        templateUrl: "tmpls/names/directives/definition.html",
+        link: function(scope) {
           if (!$stateParams.entry) {
-            scope.word.definitions = [{
-              content: '',
-              englishTranslation: '',
-              examples: []
-            }];
+            scope.word.definitions = [
+              {
+                content: "",
+                englishTranslation: "",
+                examples: []
+              }
+            ];
           }
 
-          scope.add_definition = function () {
+          scope.add_definition = function() {
             return scope.word.definitions.push({
-              content: '',
-              englishTranslation: '',
+              content: "",
+              englishTranslation: "",
               examples: []
             });
           };
 
-          scope.remove_definition = function (index) {
+          scope.remove_definition = function(index) {
             scope.word.definitions.splice(index, 1);
             if (scope.word.definitions.length < 1)
               return scope.word.definitions.push({
-                content: '',
-                englishTranslation: '',
+                content: "",
+                englishTranslation: "",
                 examples: []
               });
           };
 
-          scope.add_example = function (definition) {
+          scope.add_example = function(definition) {
             return definition.examples.push({
-              content: '',
-              englishTranslation: '',
-              type: ''
+              content: "",
+              englishTranslation: "",
+              type: ""
             });
           };
 
-          scope.remove_example = function (definition, index) {
+          scope.remove_example = function(definition, index) {
             return definition.examples.splice(index, 1);
           };
 
-          scope.$watch('word.definitions', function () {
-            scope.form.$dirty = true;
-          }, true);
+          scope.$watch(
+            "word.definitions",
+            function() {
+              scope.form.$dirty = true;
+            },
+            true
+          );
         }
       };
     }
   ])
-  .directive('multimedia', [
-    '$stateParams',
-    function ($stateParams) {
+  .directive("multimedia", [
+    "$stateParams",
+    function($stateParams) {
       return {
         replace: true,
-        restrict: 'E',
-        templateUrl: 'tmpls/names/directives/multimedia.html',
-        link: function (scope) {
+        restrict: "E",
+        templateUrl: "tmpls/names/directives/multimedia.html",
+        link: function(scope) {
           if (!$stateParams.entry) {
             scope.word.mediaLinks = [];
           }
 
-          scope.add_media = function () {
+          scope.add_media = function() {
             return scope.word.mediaLinks.push({
-              link: '',
-              caption: '',
-              type: ''
+              link: "",
+              caption: "",
+              type: ""
             });
           };
 
-          scope.remove_media = function (index) {
+          scope.remove_media = function(index) {
             scope.word.mediaLinks.splice(index, 1);
           };
 
-          scope.$watch('word.mediaLinks', function () {
-            scope.form.$dirty = true;
-          }, true);
+          scope.$watch(
+            "word.mediaLinks",
+            function() {
+              scope.form.$dirty = true;
+            },
+            true
+          );
         }
       };
     }
   ])
-  .directive('variants', [
-    '$stateParams', 'geolocationService',
-    function ($stateParams, geolocationService) {
+  .directive("variants", [
+    "$stateParams",
+    "geolocationService",
+    function($stateParams, geolocationService) {
       return {
         replace: true,
-        restrict: 'E',
-        templateUrl: 'tmpls/names/directives/variants.html',
-        link: function (scope) {
-          geolocationService.load().then(function(geolocation){
+        restrict: "E",
+        templateUrl: "tmpls/names/directives/variants.html",
+        link: function(scope) {
+          geolocationService.load().then(function(geolocation) {
             scope.geolocationList = geolocation.data;
           });
 
@@ -169,59 +185,66 @@ angular.module('NamesModule')  // Directive adds the geolocation autocompletes o
             scope.word.variants = [];
           }
 
-          scope.add_variant = function () {
+          scope.add_variant = function() {
             return scope.word.variants.push({
-              name: ''
+              name: ""
             });
           };
 
-          scope.remove_variant = function (index) {
+          scope.remove_variant = function(index) {
             scope.word.variants.splice(index, 1);
           };
 
-          scope.$watch('word.variants', function () {
-            scope.form.$dirty = true;
-          }, true);
+          scope.$watch(
+            "word.variants",
+            function() {
+              scope.form.$dirty = true;
+            },
+            true
+          );
         }
       };
     }
   ])
-  .directive('feedback', [
-    'NamesService',
-    '$modal',
-    '$stateParams',
-    '$rootScope',
-    function (api, $modal, $stateParams, $rootScope) {
+  .directive("feedback", [
+    "NamesService",
+    "$modal",
+    "$stateParams",
+    "$rootScope",
+    function(api, $modal, $stateParams, $rootScope) {
       return {
         //replace: true,
-        restrict: 'EA',
-        templateUrl: 'tmpls/names/feedbacks.html',
-        link: function (scope, element, attributes) {
-          api.getFeedback($stateParams.entry, function (resp) {
+        restrict: "EA",
+        templateUrl: "tmpls/names/feedbacks.html",
+        link: function(scope, element, attributes) {
+          api.getFeedback($stateParams.entry, function(resp) {
             scope.feedbacks = resp;
           });
-          scope.showFeedbacks = function () {
+          scope.showFeedbacks = function() {
             $modal.open({
-              templateUrl: 'tmpls/names/partials/feedbackModal.html',
-              size: 'md',
-              controller: function ($scope, $modalInstance) {
-                $scope.modalTitle = 'Feedbacks on ' + attributes.name;
+              templateUrl: "tmpls/names/partials/feedbackModal.html",
+              size: "md",
+              controller: function($scope, $modalInstance) {
+                $scope.modalTitle = "Feedbacks on " + attributes.word;
                 $scope.feedbacks = scope.feedbacks;
                 $scope.isAdmin = $rootScope.isAdmin;
                 // delete all feedbacks and closes modal
-                $scope.deleteFeedbacks = function () {
-                  api.deleteFeedbacks(attributes.name, function () {
+                $scope.deleteFeedbacks = function() {
+                  api.deleteFeedbacks(attributes.word, function() {
                     $modalInstance.close();
                   });
                 };
                 // delete one feedback by id and remove from list
-                $scope.deleteFeedback = function (feedback) {
-                  api.deleteFeedback(feedback, function () {
-                    $scope.feedbacks.splice($scope.feedbacks.indexOf(feedback), 1);
+                $scope.deleteFeedback = function(feedback) {
+                  api.deleteFeedback(feedback.id, function() {
+                    $scope.feedbacks.splice(
+                      $scope.feedbacks.indexOf(feedback),
+                      1
+                    );
                   });
                 };
-                $scope.cancel = function () {
-                  $modalInstance.dismiss('cancel');
+                $scope.cancel = function() {
+                  $modalInstance.dismiss("cancel");
                 };
               }
             });
--- app/scripts/app/names/NamesState.js
+++ app/scripts/app/names/NamesState.js
@@ -1,67 +1,60 @@
-'use strict';
+"use strict";
 
 /* States */
-angular.module('NamesModule').config([
-  '$stateProvider',
-  function ($stateProvider) {
+angular.module("NamesModule").config([
+  "$stateProvider",
+  function($stateProvider) {
     // State Configurations
     $stateProvider
-
-    // Names (parent state)
-      .state('auth.names', {
+      // Names (parent state)
+      .state("auth.names", {
         abstract: true,
-        url: '/names',
-        template: '<div ui-view autoscroll="false" class="mainView-animate"></div>',
+        url: "/names",
+        template:
+          '<div ui-view autoscroll="false" class="mainView-animate"></div>',
         data: {
           requiresBasicPriviledge: true
         }
       })
-
-    // Names > New Entries
-      .state('auth.names.add_entries', {
-        page_title: 'Yoruba Names - Admin - Add Name Entries',
-        ncyBreadcrumb: { label: 'Add Name Entries' },
-        url: '/new',
-        templateUrl: 'tmpls/names/new.html',
-        controller: 'NamesAddEntriesCtrl'
+      // Names > New Entries
+      .state("auth.names.add_entries", {
+        page_title: "Yoruba Names - Admin - Add Name Entries",
+        ncyBreadcrumb: { label: "Add Name Entries" },
+        url: "/new",
+        templateUrl: "tmpls/names/new.html",
+        controller: "NamesAddEntriesCtrl"
       })
-
-    // Edit Name Entry
-      .state('auth.names.edit_entries', {
-        page_title: 'Yoruba Names - Admin - Edit Entry',
-        ncyBreadcrumb: { label: 'Edit Entry' },
-        url: '/edit/:entry',
-        templateUrl: 'tmpls/names/edit.html',
-        controller: 'namesEditEntryCtrl'
+      // Edit Name Entry
+      .state("auth.names.edit_entries", {
+        page_title: "Yoruba Names - Admin - Edit Entry",
+        ncyBreadcrumb: { label: "Edit Entry" },
+        url: "/edit/:entry",
+        templateUrl: "tmpls/names/edit.html",
+        controller: "namesEditEntryCtrl"
       })
-
-    // Names > Published Names
-      .state('auth.names.list_entries', {
-        page_title: 'Yoruba Names - Admin - Names',
-        ncyBreadcrumb: { label: 'Names Entries' },
-        url: '/lists/:status?:submmittedBy',
-        templateUrl: 'tmpls/names/lists.html',
-        controller: 'namesListEntriesCtrl'
+      // Names > Published Names
+      .state("auth.names.list_entries", {
+        page_title: "Yoruba Names - Admin - Names",
+        ncyBreadcrumb: { label: "Names Entries" },
+        url: "/lists/:status?:submmittedBy",
+        templateUrl: "tmpls/names/lists.html",
+        controller: "namesListEntriesCtrl"
       })
-
-    // Names > Name Search
-      .state('auth.names.search', {
-        page_title: 'Yoruba Names - Admin - Name Search',
-        ncyBreadcrumb: { label: 'Search' },
-        url: '/search/:entry',
-        templateUrl: 'tmpls/names/search.html',
-        controller: 'nameSearchCtrl'
+      // Names > Name Search
+      .state("auth.names.search", {
+        page_title: "Yoruba Names - Admin - Name Search",
+        ncyBreadcrumb: { label: "Search" },
+        url: "/search/:entry",
+        templateUrl: "tmpls/names/search.html",
+        controller: "nameSearchCtrl"
       })
-
-
-    // All Names Feedback
-      .state('auth.names.feedbacks', {
-        page_title: 'Yoruba Names - Admin - Feedbacks',
-        ncyBreadcrumb: { label: 'Names Feedbacks' },
-        url: '/feedbacks',
-        templateUrl: 'tmpls/names/feedbacks-list.html',
-        controller: 'namesFeedbacksCtrl'
+      // All Names Feedback
+      .state("auth.names.feedbacks", {
+        page_title: "Yoruba Names - Admin - Feedbacks",
+        ncyBreadcrumb: { label: "Word Feedbacks" },
+        url: "/feedbacks",
+        templateUrl: "tmpls/names/feedbacks-list.html",
+        controller: "namesFeedbacksCtrl"
       });
-
   }
 ]);
